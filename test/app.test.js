af10d5b96e4a7cac6a6a4bef37c4131ac34f6a16
Michele Orselli <michele.orselli@flowing.it>
2021-03-18 12:25:29 +0100

    adds test

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('test app rest endpoints', () => {
+
+    test('It should return Hello World', async () => {
+        const response = await request(app).get('/')
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual('Hello World!!!')
+    })
+
+})
