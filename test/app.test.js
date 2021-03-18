399321537500dd68ced26ba4595d338d711309ff
Michele Orselli <michele.orselli@flowing.it>
2021-03-18 12:50:57 +0100

    hello banana!

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,7 @@ describe('test app rest endpoints', () => {
         const response = await request(app).get('/')
 
         expect(response.statusCode).toBe(200)
-        expect(response.body).toEqual('Hello World!!!')
+        expect(response.body).toEqual('Hello Banana!!!')
     })
 
 })
